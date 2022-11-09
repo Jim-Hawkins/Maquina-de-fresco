368148fea3d9e4a894d86b70fb52749bfe5721fb
Jim-Hawkins <sergiogilnova2001@gmail.com>
2022-11-09 10:40:43 +0100

    prueba satisfactoria

---
+++ NewCode/NewCode/Secrets.cs
@@ -0,0 +1,10 @@
+﻿namespace NewCode {
+    internal class Secrets {
+
+        //WiFi NAME 
+        public const string WIFI_NAME = "AndroidAP3521";
+
+        //PASSWORD FOR THE WiFi NAME
+        public const string WIFI_PASSWORD = "72a0f6d02d93";
+    }
+}
