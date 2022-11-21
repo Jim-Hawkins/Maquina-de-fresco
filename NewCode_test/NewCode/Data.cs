c01fb42f0709289d3f39c6acd5555832b6ae2ad2
Jim-Hawkins <sergiogilnova2001@gmail.com>
2022-11-21 17:29:44 +0100

    webserver casi funciona y rangos también

---
+++ NewCode_test/NewCode/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Meadow.Foundation;
+
+namespace NewCode {
+    class Data {
+
+        //WEB VARIABLES
+        public static string IP = null;
+        public static int Port = 2550;
+
+        //ROUND VARIABLES
+        public static string[] temp_max = { "18", "30" }; // In ºC
+        public static string[] temp_min = { "16", "12" }; // In ºC
+        public static int display_refresh = 100; // In ms
+        public static int refresh = 100; // In ms
+        public static String[] round_time = { "60", "5" }; // in s
+        public static int current_round = 0;
+
+        //START ROUND VARIABLES
+        public static bool is_working = false;
+        public static string temp_act = "0"; // In ºC
+        public static int time_left; // in s
+        public static int time_in_range_temp = 0; //In ms.
+                                           
+        //COLORS FOR DISPLAY
+        public static Color[] colors = new Color[4]
+        {
+            Color.FromHex("#67E667"),
+            Color.FromHex("#00CC00"),
+            Color.FromHex("#269926"),
+            Color.FromHex("#008500")
+        };
+
+        //RELAY VARIABLES
+        public static bool on = true; // enfriando
+    }
+}
---
+++ NewCode_test/NewCode/Secrets.cs
@@ -0,0 +1,12 @@
+﻿namespace NewCode {
+    internal class Secrets {
+
+        //WiFi NAME 
+        //public const string WIFI_NAME = "KCS_CORPORATE";
+        public const string WIFI_NAME = "AndroidAP3521";
+
+        //PASSWORD FOR THE WiFi NAME
+        //public const string WIFI_PASSWORD = "E7289DHTYdwegeyswe";
+        public const string WIFI_PASSWORD = "72a0f6d02d93";
+    }
+}
